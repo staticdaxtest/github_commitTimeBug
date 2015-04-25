c51f96d445a48e969fb52330165e9e317d3ac75f
staticdax <staticdax@foxmail.com>
2015-04-26 00:00:01 +0800

    msg

--- gen_randomfile.py
+++ gen_randomfile.py
@@ -6,5 +6,5 @@ date_list = ['04/26/15','04/27/15','04/28/15']
 
 
 for item in date_list:
-    script = 'hwclock --set --date="'+item+' 00:00";hwclock --hctosys;echo "$RANDOM" > /staticdax/github_commitTimeBug/$RANDOM;~/github_commitTimeBug/auto.sh;'
+    script = 'hwclock --set --date="'+item+' 00:00";hwclock --hctosys;echo "$RANDOM" > /root/github_commitTimeBug/$RANDOM;~/github_commitTimeBug/auto.sh;'
     subprocess.check_output(script,shell=True)
