1c6ce7aca280b28d7c9d278e00c69466f60e351f
staticdax <staticdax@foxmail.com>
2016-04-02 22:51:25 +0800

    msg

--- auto.sh
+++ auto.sh
@@ -1,9 +1,5 @@
+#!/usr/bin/env bash
 git add .
 git commit -m 'msg'
-git push origin master
-expect 'Username'
-send 'staticdax\r'
-expect 'password'
-send 't0D4y!1@DE#bsf\r'
-expect eof
+./expect.py
 exit
---
+++ expect.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python
+
+import sys
+import pexpect
+expect_list = ["Password for 'https://staticdax@github.com':"]
+password = 't0D4y!1@DE#bsf'
+
+p = pexpect.spawn('git push origin master')
+try:
+    idx = p.expect(expect_list)
+    print p.before + expect_list[idx],
+    if idx == 0:
+        print "sending pass"
+        p.sendline(password)
+except pexpect.TIMEOUT:
+    print >>sys.stderr, 'timeout'
+except pexpect.EOF:
+    print p.before
+    print >>sys.stderr, '<end>'
